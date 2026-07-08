981b615c7a3dc673a56b0caf9ec5286ddbac9a22
RaiFlores05 <rflores@byupathway.edu>
2026-07-07 21:47:06 -0500

    Finished Exercise W02

---
+++ week02/scripts/bom.js
@@ -0,0 +1,32 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+const li = document.createElement('li');
+
+const deleteButton = document.createElement('button');
+
+
+
+button.addEventListener('click',function(){
+    const li = document.createElement('li');
+    const deleteButton = document.createElement('button');
+
+    li.textContent = input.value;
+    deleteButton.textContent = 'X';
+    li.append(deleteButton);
+    list.append(li);
+    if (input.value.trim() !==''){ 
+        li.append(deleteButton);
+        list.append(li);
+     }
+
+     deleteButton.addEventListener('click', function(){
+    list.removeChild(li);
+    input.focus();
+});
+
+
+
+input.value = '';
+input.focus();})
